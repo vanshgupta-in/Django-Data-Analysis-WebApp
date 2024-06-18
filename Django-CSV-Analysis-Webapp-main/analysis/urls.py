6d22ed6cc8417e72dc9fb34f32f5c62c84b70f7b
vanshgupta-in <vanshgupta2744@gmail.com>
2024-06-18 12:47:26 +0530

    Delete Django-CSV-Analysis-Webapp-main directory

--- Django-CSV-Analysis-Webapp-main/analysis/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.upload_file, name='upload_file'),
-    path('analyze/<int:file_id>/', views.analyze_file, name='analyze_file'),
-]
